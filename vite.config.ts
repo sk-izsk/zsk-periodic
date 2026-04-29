a1eaf3d1c1142b6747e603077e83b39c85a96714
Zeeshan <sk.zeeshan1992@gmail.comn>
2026-04-28 23:40:23 -0400

    fix duplication

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
       routesDirectory: './src/routes',
       generatedRouteTree: './src/routeTree.gen.ts',
     }),
